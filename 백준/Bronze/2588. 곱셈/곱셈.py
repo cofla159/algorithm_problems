cf7aac298773675da9805ad509c0995ce0a730ec
황채림 <70076564+cofla159@users.noreply.github.com>
2023-03-10 03:10:11 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+
+mul_1 = int(a)*int(b[2])
+mul_2 = int(a)*int(b[1])
+mul_3 = int(a)*int(b[0])
+answer = mul_1+mul_2*10+mul_3*100
+print(mul_1)
+print(mul_2)
+print(mul_3)
+print(answer)
