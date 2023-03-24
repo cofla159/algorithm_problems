27c6914dc674ac73ec4fa7fa79731ccc03156740
황채림 <70076564+cofla159@users.noreply.github.com>
2023-03-24 21:33:04 +0900

    [Gold V] Title: 동전, Time: 76 ms, Memory: 33372 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,15 +1,30 @@
-from sys import stdin
+# 9084 동전
+import sys
+input = sys.stdin.readline
 
-t = int(stdin.readline())
-for _ in range(t):
-    n = int(stdin.readline())
-    coins = list(map(int, stdin.readline().split()))
-    m = int(stdin.readline())
-    cases = [0]*(m+1)
-    cases[0] = 1
-    for price in coins:
-        for i in range(1, m+1):
-            if i-price >= 0:
-                cases[i] += cases[i-price]
 
-    print(cases[m])
+def dp(m, i):
+    """0번째 동전부터 i번째 동전까지 사용해 m을 만들 수 있는 경우의 수"""
+    if m < 0:
+        return 0
+    if value_case[i][m] is not None:
+        return value_case[i][m]
+    if i == 0:  # 사용할 수 있는 동전이 끝의 동전 하나일 때
+        if m % coins[i] == 0:  # 하나의 동전으로 나누어 떨어지면
+            value_case[i][m] = 1
+        else:
+            value_case[i][m] = 0
+    else:
+        now_coin = coins[i]
+        value_case[i][m] = dp(m, i-1) + dp(m-now_coin, i)  # i번째 동전을 쓰거나 안 쓰거나
+    return value_case[i][m]
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    coins = tuple(map(int, input().split()))
+    M = int(input())
+    value_case = [[None]*(M+1) for _ in range(N)]
+
+    print(dp(M, N-1))
