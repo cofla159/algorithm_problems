73aec9f7a4e30a2d8fd53e58051e5b1f55f4eb37
황채림 <70076564+cofla159@users.noreply.github.com>
2023-04-03 10:18:46 +0900

    [Silver V] Title: 단어 정렬, Time: 84 ms, Memory: 34332 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,17 +1,16 @@
-n = int(input())
-words = {}
-max = 0
-for _ in range(n):
-    word = input()
-    length = len(word)
-    if words.get(length) == None:
-        words[length] = set()
-    words[length].add(word)
-    if length > max:
-        max = length
+from sys import stdin
 
-for i in range(1, max+1):
-    if words.get(i) != None:
-        new_list = sorted(list(words[i]))
-        for j in range(len(new_list)):
-            print(new_list[j])
+n = int(stdin.readline())
+words = {}
+max_length = 0
+for _ in range(n):
+    input_word = stdin.readline().rstrip()
+    word_len = len(input_word)
+    if not words.get(word_len):
+        words[word_len] = set()
+    words[word_len].add(input_word)
+    max_length = max(max_length, word_len)
+
+for i in range(1, max_length+1):
+    if words.get(i):
+        print(*sorted(list(words[i])), sep='\n')
