6c06906885fdd882471848c0f2a3572c82dd9ec9
황채림 <70076564+cofla159@users.noreply.github.com>
2023-04-07 10:34:11 +0900

    [Gold V] Title: 탑, Time: 600 ms, Memory: 102076 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,17 +1,21 @@
 from sys import stdin
+from collections import deque
 
 n = int(stdin.readline())
 towers = list(map(int, stdin.readline().split()))
-towers.reverse()
-
-stack = [0]
-answer = [0]*n
-
-for i in range(1, n):
-    while len(stack) > 0 and towers[i] > towers[stack[-1]]:
-        last = stack.pop()
-        answer[last] = n-i
+answer = []
+stack = deque()
+for i in range(n):
+    flag = True
+    while stack:
+        if towers[stack[-1]] > towers[i]:
+            answer.append(stack[-1]+1)
+            flag = False
+            break
+        else:
+            stack.pop()
+    if flag:
+        answer.append(0)
     stack.append(i)
 
-
-print(*reversed(answer))
+print(*answer)
