7f4bc2b71dad5cb450ce031d032a3b779fab1570
황채림 <70076564+cofla159@users.noreply.github.com>
2023-07-16 21:05:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    const oneCount = n.toString(2).match(/1/g).length;
+    while(n++){
+        if (n.toString(2).match(/1/g).length === oneCount) break;
+    }
+    return n;
+}
