c3625f595f0fc484ab8b15c4da90d4d969bea230
황채림 <70076564+cofla159@users.noreply.github.com>
2023-10-20 06:32:28 +0900

    [level 2] Title: 올바른 괄호, Time: 5.43 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,10 @@
+function solution(s){
+    if (s[0] === ")") return false;
+    const stack = [];
+    
+    for (let i = 0; i< s.length; i++){
+        if ((stack[stack.length-1] === "(" && s[i] === ")") ) stack.pop();
+        else stack.push(s[i]);
+    }
+    return stack.length === 0;
+}
